fc0b5c6a2959f55fb243a6a759f9fe7675acae43
elbow667 <elbow667@gmail.com>
2022-11-22 11:24:53 -0500

    Tutorial 7

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,14 +4,10 @@ class_name Player
 #var velocity = Vector2.ZERO
 # velocity already defined in CharacterBody2D
 #@export var SPEED : float = 100.0
-@export var JUMP_FORCE : int = -130
-@export var JUMP_RELEASE_FORCE: int = -70
-@export var MAX_SPEED : int = 50
-@export var FRICTION : int = 10
-@export var ACCELLERATION : int = 10
-@export var GRAVITY : int = 4
-@export var ADDITIONAL_FALL_GRAVITY : int = 4
+
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+@export var moveData : Resource
 
 func _ready():
 	animated_sprite.frames = load("res://Scenes/player_pink_skin.tres")
@@ -37,13 +33,13 @@ func _physics_process(_delta):
 	if is_on_floor():
 
 		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_FORCE
+			velocity.y = moveData.JUMP_FORCE
 	else:
 		animated_sprite.animation = "Jump"
-		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_RELEASE_FORCE:
-			velocity.y = JUMP_RELEASE_FORCE
+		if Input.is_action_just_released("ui_accept") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+			velocity.y = moveData.JUMP_RELEASE_FORCE
 		if velocity.y > 0:
-			velocity.y += ADDITIONAL_FALL_GRAVITY
+			velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
 
 
 	var was_in_air = not is_on_floor()
@@ -54,13 +50,13 @@ func _physics_process(_delta):
 		animated_sprite.frame = 1
 		
 func apply_gravity():
-	velocity.y += GRAVITY
+	velocity.y += moveData.GRAVITY
 	velocity.y = min(velocity.y, 300)
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, FRICTION)
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
 	
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELLERATION)
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELLERATION)
 	
 	
---
+++ Scripts/player_movement_data.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name PlayerMovmentData
+
+@export var JUMP_FORCE : int = -160
+@export var JUMP_RELEASE_FORCE: int = -70
+@export var MAX_SPEED : int = 75
+@export var FRICTION : int = 10
+@export var ACCELLERATION : int = 10
+@export var GRAVITY : int = 5
+@export var ADDITIONAL_FALL_GRAVITY : int = 2
