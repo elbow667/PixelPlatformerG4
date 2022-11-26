caf8de1b601e0472c67991ec50c68530d9f7c682
elbow667 <elbow667@gmail.com>
2022-11-26 17:42:16 -0500

    Tutorial 10 (Looping Path Enemies)

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PixelPlatformerG4
 Heartbeast tutorial for Pixel Platformer but created in Godot 4.0 beta.<br>
+for PathFollow2d properties Offset and Unit Offset have been renamed to Progress <br> 
+and Progress Ratio <br>
 @export var moveData: PlayerMovmentData was sufficient to get autocompletion in 4.0 <br>
 On LadderCheck Raycast2D: make sure property "Hit from Inside" is selected. <br>
 velocity appears to be already defined and not needing to be passed into <br>
---
+++ Scripts/moving_spike_enemy.gd
@@ -0,0 +1,42 @@
+#@tool
+# tool stuff doesn't seem to be working in Godot 4.0
+# setget is implemented differently in Godot 4.0, since its not working
+# and none of the examples I have seeked out have worked for this so I have 
+# no idea if i'm doing it correctly so I have commneted it out.
+
+extends Path2D
+
+enum ANIMATION_TYPE {
+	LOOP, 
+	BOUNCE
+}
+
+@export var animation_type : ANIMATION_TYPE #:
+#	get:
+#		return animation_type
+#	set(value):
+#		set_animation_type(value)
+#
+#func set_animation_type(value):
+#	animation_type = value
+#	play_updated_animation(animationPlayer)
+#@export var speed : int = 1:
+#	get:
+#		return speed
+#	set(value):
+#		var ap = find_child("AnimationPlayer")
+#		if ap: ap.playback_speed = speed
+		
+
+@onready var animationPlayer : AnimationPlayer = $AnimationPlayer
+
+
+func _ready():
+	play_updated_animation(animationPlayer)
+
+func play_updated_animation(animationPlayer):
+	match animation_type:
+		ANIMATION_TYPE.LOOP: animationPlayer.play("MoveAlongPathLoop")
+		ANIMATION_TYPE.BOUNCE: animationPlayer.play("MoveAlongPathBounce")
+		
+		
