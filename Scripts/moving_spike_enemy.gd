c9fa11f47c2d866842743b79d55c9476b2574529
elbow667 <elbow667@gmail.com>
2022-11-27 18:30:01 -0500

    fixed setter in tool script with await ready

--- Scripts/moving_spike_enemy.gd
+++ Scripts/moving_spike_enemy.gd
@@ -6,32 +6,35 @@ enum ANIMATION_TYPE {
 	LOOP, 
 	BOUNCE
 }
+@onready var animationPlayer : = $AnimationPlayer
 
 @export var animation_type : ANIMATION_TYPE:
-	get:
-		return animation_type
+#	get:
+#		return animation_type
 	set(value):
 		animation_type = value
+		if not is_inside_tree():
+			await ready
 		var ap = get_node("AnimationPlayer")
 		if ap: play_updated_animation(ap)
 
+
 @export var speed = 1:
-	get:
-		return speed
+#	get:
+#		return speed
 	set(value):
 		speed = value
+		if not is_inside_tree():
+			await ready		
 		var ap = get_node("AnimationPlayer")
 		if ap: ap.playback_speed = speed
-
-@onready var animationPlayer : AnimationPlayer = $AnimationPlayer
-
 
 func _ready():
 	play_updated_animation(animationPlayer)
 
-func play_updated_animation(animationPlayer):
+func play_updated_animation(ap):
 	match animation_type:
-		ANIMATION_TYPE.LOOP: animationPlayer.play("MoveAlongPathLoop")
-		ANIMATION_TYPE.BOUNCE: animationPlayer.play("MoveAlongPathBounce")
+		ANIMATION_TYPE.LOOP: ap.play("MoveAlongPathLoop")
+		ANIMATION_TYPE.BOUNCE: ap.play("MoveAlongPathBounce")
 		
 		
