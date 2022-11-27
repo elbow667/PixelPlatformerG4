c3c3916f31fae3e87619be65b58fdcb59656f171
elbow667 <elbow667@gmail.com>
2022-11-27 12:06:12 -0500

    fixed tool code setters for 4.0

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Heartbeast tutorial for Pixel Platformer but created in Godot 4.0 beta.<br>
 * For PathFollow2d properties Offset and Unit Offset have been renamed to Progress <br> 
 	and Progress Ratio <br>
 * setget seems to have been removed and implimented differently in Godot 4.0, <br>
-	as a result, I have for the time being removed the tool code for moving_spike_enemy.gd <br>
+	as a result, the tool code for moving_spike_enemy.gd looks quite different. <br>
 * @export var moveData: PlayerMovmentData was sufficient to get autocompletion in 4.0 <br>
 * On LadderCheck Raycast2D: make sure property "Hit from Inside" is selected. <br>
 * velocity appears to be already defined and not needing to be passed into <br>
--- Scripts/moving_spike_enemy.gd
+++ Scripts/moving_spike_enemy.gd
@@ -1,8 +1,4 @@
-#@tool
-# tool stuff doesn't seem to be working in Godot 4.0
-# setget is implemented differently in Godot 4.0, since its not working
-# and none of the examples I have seeked out have worked for this so I have 
-# no idea if i'm doing it correctly so I have commented it out.
+@tool
 
 extends Path2D
 
@@ -11,22 +7,21 @@ enum ANIMATION_TYPE {
 	BOUNCE
 }
 
-@export var animation_type : ANIMATION_TYPE #:
-#	get:
-#		return animation_type
-#	set(value):
-#		set_animation_type(value)
-#
-#func set_animation_type(value):
-#	animation_type = value
-#	play_updated_animation(animationPlayer)
-#@export var speed : int = 1:
-#	get:
-#		return speed
-#	set(value):
-#		var ap = find_child("AnimationPlayer")
-#		if ap: ap.playback_speed = speed
-		
+@export var animation_type : ANIMATION_TYPE:
+	get:
+		return animation_type
+	set(value):
+		animation_type = value
+		var ap = get_node("AnimationPlayer")
+		if ap: play_updated_animation(ap)
+
+@export var speed = 1:
+	get:
+		return speed
+	set(value):
+		speed = value
+		var ap = get_node("AnimationPlayer")
+		if ap: ap.playback_speed = speed
 
 @onready var animationPlayer : AnimationPlayer = $AnimationPlayer
 
