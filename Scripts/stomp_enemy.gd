5207736bdd1672c35c9029c550e1f4dd3b142b79
elbow667 <elbow667@gmail.com>
2022-12-01 22:22:39 -0500

    Tutorial 13 StompEnemy

--- Scripts/stomp_enemy.gd
+++ Scripts/stomp_enemy.gd
@@ -7,19 +7,34 @@ var state = HOVER
 @onready var start_position = global_position
 @onready var timer: = $Timer
 @onready var raycast: = $RayCast2D
+@onready var animatedSprite: = $AnimatedSprite2D
 
 func _physics_process(delta):
 	match state:
-		HOVER: pass
-		FALL: pass
-		LAND: pass
-		RISE: pass
+		HOVER: hover_state()
+		FALL: fall_state(delta)
+		LAND: land_state()
+		RISE: rise_state(delta)
 
 func hover_state():
 	state = FALL
 
-func fall_state():
-	position.y += 10
+func fall_state(delta):
+	animatedSprite.play("Falling")
+	position.y += 100 * delta
 	if raycast.is_colliding():
 		var collision_point = raycast.get_collision_point()
 		position.y = collision_point.y
+		state = LAND
+		timer.start(1.0)
+		
+func land_state():
+	if timer.time_left == 0:
+		state = RISE
+	
+func rise_state(delta):
+	animatedSprite.play("Rising")
+	position.y = move_toward(position.y, start_position.y,20 * delta)
+	if position.y == start_position.y:
+		state = HOVER
+	
