07c2350c0520e12e0228d7b53653966791469731
elbow667 <elbow667@gmail.com>
2022-12-13 21:55:14 -0500

    Tutorial 16 (Level Transitions)

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -8,4 +8,9 @@ extends Area2D
 func _on_body_entered(body):
 	if not body is Player: return
 	if target_level_path.is_empty(): return
+	Transitions.play_exit_transition()
+	get_tree().paused = true
+	await Transitions.transition_completed
+	Transitions.play_enter_transition()
+	get_tree().paused = false
 	get_tree().change_scene_to_file(target_level_path)
